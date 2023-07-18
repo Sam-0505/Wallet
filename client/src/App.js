407ef2d448f04841ee24c5b65cfb68bde0f3218b
admin <admin@Samir-MacBook-Pro.local>
2023-07-18 20:30:47 +0530

    Created register and login page

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/register/register'
+import Register from './components/register/register';
+import Login from './components/login/login';
+
+function App() {
+  return (
+    <Register></Register>,
+    <Login></Login>
+  );
+}
+
+export default App;
