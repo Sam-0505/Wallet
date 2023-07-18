6df54e14706ffdd21a1698e59f28b2a34d681812
admin <admin@Samir-MacBook-Pro.local>
2023-07-18 19:25:02 +0530

    Created mongo databse

--- server/app.js
+++ server/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 
 const app = express();
+
+mongoose
+    .connect(process.env.URI)
+    .then(()=>{
+        console.log("Connected successfully");
+        app.listen(process.env.PORT || 8000,(err)=>{
+            if(err)console.log(err);
+            console.log(`running at port ${process.env.PORT}`);
+        })
+    })
+    .catch((error)=>console.log("Failed to connect",error));
 
 app.get("/",(req,res)=>{
     res.send("api running");
 });
 
-app.listen(3000);
+//app.listen(3000);
