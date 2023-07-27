5ce6d694cf8dab98f191c930ac3eedecc7c30263
admin <admin@Samir-MacBook-Pro.local>
2023-07-27 11:32:52 +0530

    Suggestions are added on the search box

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,155 +1,204 @@
-import { useState, useContext, useEffect} from "react";
+import { useState, useContext, useEffect } from "react";
 import { UserContext } from "../userContext";
-import axios from "axios"
-import "./dashboard.css"
-import { useNavigate } from "react-router-dom"
+import axios from "axios";
+import "./dashboard.css";
+import { useNavigate } from "react-router-dom";
 
-export default function Dashboard(){
-    const {globUser,setGlobUser} = useContext(UserContext);
-    const [currBal, setCurrBal] = useState(globUser.balance);
-    const [transData, setTransData] = useState([]);
+export default function Dashboard() {
+  const { globUser, setGlobUser } = useContext(UserContext);
+  const [currBal, setCurrBal] = useState(globUser.balance);
+  const [sug, setSug] = useState([]);
+  const [transData, setTransData] = useState([]);
 
-    const [limit, setLimit] = useState(0);
-    const [direc, setDirec] = useState(0);
-    const [collectionData, setCollectionData] = useState([]);
+  const [limit, setLimit] = useState(0);
+  const [direc, setDirec] = useState(0);
+  const [collectionData, setCollectionData] = useState([]);
 
-    const nav = useNavigate();
+  const nav = useNavigate();
 
-    const [send, setSend] = useState({
-        sendEmail:"",
-        sendAmount:0
-    })
+  const [send, setSend] = useState({
+    sendEmail: "",
+    sendAmount: 0,
+  });
 
-    //console.log(globUser);
+  //console.log(globUser);
 
-    const handleChange = e =>{
-        const { name, value } = e.target
-        setSend({
-            ...send,
-            [name]: value
-        })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSend({
+      ...send,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    var res = [];
+    for (var i = 0; i < transData.length; i++) {
+      if (
+        transData[i].destination != globUser.email &&
+        res.indexOf(transData[i].destination) == -1 &&
+        transData[i].destination.startsWith(send.sendEmail) &&
+        res.length < 5
+      ) {
+        res.push(transData[i].destination);
+      }
     }
 
-    function sendMoney()
-    {
-        axios.post("http://localhost:9002/sendMoney", {globUser, send},{ withCredentials: true })
-        .then(res => {
-            if(res.data==="Transaction Completed")
-            {
-                setCurrBal(currBal-send.sendAmount);
-            }
-            alert(res.data)
-        })
-    }
+    setSug(res);
+  }, [send]);
 
-    useEffect(()=>{
-        axios.post("http://localhost:9002/showTrans",{globUser},{ withCredentials: true })
-        .then(res => {
-            setTransData(res.data);
-            setCollectionData(res.data)
-        })
-    },[globUser,currBal]);
-
-    function logOut()
-    {
-        setGlobUser(null);
-        localStorage.clear();
-        nav('/');
-    }
-
-    function getDay(date)
-    {
-        return date.split('T')[0];
-    }
-
-    function getTime(date)
-    {
-        var slots = date.split('T')[1].split('.')[0].split(':');
-        var min = Number(slots[1])+30;
-        var hour = Number(slots[0])+5+parseInt(min/60);
-        min = min%60;
-
-        return ""+hour+":"+min+":"+slots[2];
-    }
-
-    const filterAmount = (e) =>{
-        var limit = e.target.value;
-        setLimit(limit);
-        const result = transData.filter(trans => trans.amount>limit);
-        setCollectionData(result);
-    }
-
-    const filterDirection=(e)=>{
-        var pt = e.target.value;
-        setDirec(pt);
-    }
-
-    useEffect(()=>{
-        var res;
-
-        if(direc==0)
-        {
-            res=transData;
+  function sendMoney() {
+    //console.log(sug);
+    axios
+      .post(
+        "http://localhost:9002/sendMoney",
+        { globUser, send },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        if (res.data === "Transaction Completed") {
+          setCurrBal(currBal - send.sendAmount);
         }
-        else if(direc==1)
-        {
-            res = transData.filter(trans=>trans.source==globUser.email);
-        }
-        else
-        {
-            res = transData.filter(trans=>trans.destination==globUser.email);
-        }
+        alert(res.data);
+      });
+  }
 
-        setCollectionData(res.filter(trans=>trans.amount>limit))
-    },[limit,direc]);
+  useEffect(() => {
+    axios
+      .post(
+        "http://localhost:9002/showTrans",
+        { globUser },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        setTransData(res.data);
+        setCollectionData(res.data);
+      });
+  }, [globUser, currBal]);
 
-    return (<div className="dashboard">
-        <h1>Dashboard</h1>
-        {!! globUser && (<h2>Hi {globUser.name} !!</h2>)}
-        {!! globUser && (<h2>Balance: {currBal} !!</h2>)}
-        <input type="text" name="sendEmail" value={send.sendEmail} onChange={handleChange} placeholder="Enter Sender Email"></input>
-        <input type="text" name="sendAmount" value={send.sendAmount} onChange={handleChange} placeholder="Enter Amount" ></input>
-        <div className="button" onClick={sendMoney}>Send Money</div>
-        {/* <div className="button" onClick={showTrans}>Show Transactions</div> */}
-        <div>
-        <h1>Transaction Data</h1>
-            {"FILTERS: "}
-            {"Transaction type: "}
-            <select onChange={filterDirection}>
-                <option value="0">All transactions</option>
-                <option value="-1">Recevied</option>
-                <option value="1">Sent</option>
-            </select>
-            {" Amount: "}
-            <select onChange={filterAmount}>
-                <option value="0">All transactions</option>
-                <option value="50">Greater than 50</option>
-                <option value="30">Greater than 30</option>
-                <option value="10">Greater than 10</option>
-            </select>
-            <table cellPadding="5" cellSpacing="5">
-                <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Sender</th>
-                    <th>Receiver</th>
-                    <th>Amount</th>
-                </tr>
-                </thead>
-                <tbody>
-                {collectionData.map((item, index) => (
-                    <tr key={index}>
-                    <td>{getDay(item.createdAt)}</td>
-                    <td>{getTime(item.createdAt)}</td>
-                    <td>{item.source}</td>
-                    <td>{item.destination}</td>
-                    <td>{item.amount}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+  function logOut() {
+    setGlobUser(null);
+    localStorage.clear();
+    nav("/");
+  }
+
+  function getDay(date) {
+    return date.split("T")[0];
+  }
+
+  function getTime(date) {
+    var slots = date.split("T")[1].split(".")[0].split(":");
+    var min = Number(slots[1]) + 30;
+    var hour = Number(slots[0]) + 5 + parseInt(min / 60);
+    min = min % 60;
+
+    return "" + hour + ":" + min + ":" + slots[2];
+  }
+
+  const filterAmount = (e) => {
+    var limit = e.target.value;
+    setLimit(limit);
+    const result = transData.filter((trans) => trans.amount > limit);
+    setCollectionData(result);
+  };
+
+  const filterDirection = (e) => {
+    var pt = e.target.value;
+    setDirec(pt);
+  };
+
+  useEffect(() => {
+    var res;
+
+    if (direc == 0) {
+      res = transData;
+    } else if (direc == 1) {
+      res = transData.filter((trans) => trans.source == globUser.email);
+    } else {
+      res = transData.filter((trans) => trans.destination == globUser.email);
+    }
+
+    setCollectionData(res.filter((trans) => trans.amount > limit));
+  }, [limit, direc]);
+
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      {!!globUser && <h2>Hi {globUser.name} !!</h2>}
+      {!!globUser && <h2>Balance: {currBal} !!</h2>}
+      <input
+        type="text"
+        name="sendEmail"
+        value={send.sendEmail}
+        onChange={handleChange}
+        placeholder="Enter Sender Email"
+      ></input>
+      {sug.length != 0 && (
+        <div className="dropdown">
+          {sug.map((item, index) => (
+            <div
+              className="dropdown-row"
+              key={index}
+              onClick={(e) => console.log(e.target.textContent)}
+            >
+              {item}
+            </div>
+          ))}
         </div>
-        <div className="button" onClick={logOut}>Logout</div>
-    </div>)
+      )}
+      <input
+        type="text"
+        name="sendAmount"
+        value={send.sendAmount}
+        onChange={handleChange}
+        placeholder="Enter Amount"
+      ></input>
+      <div className="button" onClick={sendMoney}>
+        Send Money
+      </div>
+      {/* <div className="button" onClick={showTrans}>Show Transactions</div> */}
+      <div>
+        <h1>Transaction Data</h1>
+        {"FILTERS: "}
+        {"Transaction type: "}
+        <select onChange={filterDirection}>
+          <option value="0">All transactions</option>
+          <option value="-1">Recevied</option>
+          <option value="1">Sent</option>
+        </select>
+        {" Amount: "}
+        <select onChange={filterAmount}>
+          <option value="0">All transactions</option>
+          <option value="50">Greater than 50</option>
+          <option value="30">Greater than 30</option>
+          <option value="10">Greater than 10</option>
+        </select>
+        <table cellPadding="5" cellSpacing="5">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Sender</th>
+              <th>Receiver</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {collectionData.map((item, index) => (
+              <tr key={index}>
+                <td>{getDay(item.createdAt)}</td>
+                <td>{getTime(item.createdAt)}</td>
+                <td>{item.source}</td>
+                <td>{item.destination}</td>
+                <td>{item.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="button" onClick={logOut}>
+        Logout
+      </div>
+    </div>
+  );
 }
--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,57 +1,71 @@
-import React, {useState, useContext} from "react"
-import "./login.css"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
+import React, { useState, useContext } from "react";
+import "./login.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../userContext";
 
-const Login = ({ setLoginUser}) => {
+const Login = ({ setLoginUser }) => {
+  const { globUser, setGlobUser } = useContext(UserContext);
 
-    const {globUser, setGlobUser} = useContext(UserContext);
+  const nav = useNavigate();
 
-    const nav = useNavigate();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [ user, setUser] = useState({
-        email:"",
-        password:""
-    })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
 
-    const handleChange = e => {
-        const { name, value } = e.target
-        setUser({
-            ...user,
-            [name]: value
-        })
-    }
+  const login = () => {
+    axios
+      .post("http://localhost:9002/login", user, { withCredentials: true })
+      .then((res) => {
+        if (res.data != "Invalid Credentials") {
+          setGlobUser(res.data.userData);
+          localStorage.setItem("token", JSON.stringify(res.data.userToken));
+          nav("/dashboard");
+        } else {
+          alert(res.data.userData);
+        }
+      });
+  };
 
-    const login = () => {
-        axios.post("http://localhost:9002/login", user,{ withCredentials: true })
-        .then(res => {
-            if(res.data != "Invalid Credentials")
-            {
-                setGlobUser(res.data.userData);
-                localStorage.setItem('token',JSON.stringify(res.data.userToken));
-                nav("/dashboard");
-            }
-            else{
-                alert(res.data.userData);
-            }
-        })
-    }
+  const home = () => {
+    nav("/");
+  };
 
-    const home = ()=>{
-        nav("/");
-    }
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <input
+        type="text"
+        name="email"
+        value={user.email}
+        onChange={handleChange}
+        placeholder="Enter your Email"
+      ></input>
+      <input
+        type="password"
+        name="password"
+        value={user.password}
+        onChange={handleChange}
+        placeholder="Enter your Password"
+      ></input>
+      <div className="button" onClick={login}>
+        Login
+      </div>
+      <div>or</div>
+      <div className="button" onClick={home}>
+        Home
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="login">
-            <h1>Login</h1>
-            <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
-            <input type="password" name="password" value={user.password} onChange={handleChange}  placeholder="Enter your Password" ></input>
-            <div className="button" onClick={login}>Login</div>
-            <div>or</div>
-            <div className="button" onClick={home}>Home</div>
-        </div>
-    )
-}
-
-export default Login
+export default Login;
